91fe1dd65dbdd34f71136c5e3a706b82f584717e
Mustafa Erdogan <34981250+muerdogan@users.noreply.github.com>
2019-06-01 00:12:29 +0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -26,3 +26,24 @@ first name*3 #JackJackJack
 "Jack" + last name + "got" + "a" + "jacket" #JackBrowngotajacket
 
 
+#more about strings 
+
+name = "Jack Brown"
+
+print(name) #Jack Brown
+
+name[0] #index returns for "J" first letter from left to right
+
+name[7] #index returns for "o" 8th letter left to right
+
+name[-3] #index returns for "o" 3rd letter from right to left
+
+name[3:10] #index returns right to left between 4th to 10th letter : ck Brown 
+
+name[:7] #index start beginning left to right to 8th letter : Jack Bro
+
+name[2:] #index starts from right to left 3rd : ck Brown
+
+len(name) #returns for character count : 10
+
+len("python") #6
