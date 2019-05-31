1beee9b19a73d3821091a5844950cd18fc2448ad
Mustafa Erdogan <34981250+muerdogan@users.noreply.github.com>
2019-05-31 23:43:54 +0300

    Add files via upload

---
+++ python-basic-math.py
@@ -0,0 +1,25 @@
+3+7 #sum
+7-9 #subtract
+5/6 #divide
+8*3 #multiply
+
+17 // 2 #returns with intiger instead of floating-point
+
+8*3+30 #transaction privilidge starts with multiplication
+
+8*(3+30) #transaction privilidge starts inside parenthesis
+	
+17 % 4 #returns for residual after 17 divided to 4 = 1
+
+5**3 # =5*5*5
+
+#math variables :
+
+x = 5
+y = 7
+
+x*y #returns for 5*7 = 35
+
+
+
+
