a4b3b0f53145cf629add3728833f98232b737f4f
Mustafa Erdogan <34981250+muerdogan@users.noreply.github.com>
2019-05-31 23:55:33 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,28 @@
+#strings ;
+
+whatever text #will return with a syntax error
+
+'whatever text' #string
+"whatever text" #string
+"317" #string
+317 #NOT string
+
+"Mr. Brown's jacket"
+'Mr. Brown\'s jacket' #escape string
+
+
+#strings with variables ;
+
+first name = "Jack"
+last name = "Brown"
+
+first name + last name #will return with "JackBrown"
+
+first name + "Brown" #returns with "JackBrown"
+
+first name + first name + first name #JackJackJack
+first name*3 #JackJackJack
+
+"Jack" + last name + "got" + "a" + "jacket" #JackBrowngotajacket
+
+
