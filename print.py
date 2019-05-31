1b833469dc76cebe7338af38fd331c00415bbe20
Mustafa Erdogan <34981250+muerdogan@users.noreply.github.com>
2019-06-01 00:03:05 +0300

    Add files via upload

---
+++ print.py
@@ -0,0 +1,28 @@
+#print statement 
+
+first name = "Jack"
+last name = "Brown"
+
+print(first name) #returns with "Jack" string
+
+print("Jack") #returns with "Jack" string
+
+print(first name,last name) #returns with "Jack Brown"
+
+print(first name + last name) #returns with "JackBrown" without a space between first name and last name
+
+print("Jack",3) #returns "Jack 3"
+
+print("Jack",5,8.9,"Brown") #returns Jack 5 8.9 Brown
+
+print("Jack7") #returns with string "Jack7"
+
+print("Jack" +str(7)) #returns with string Jack7
+
+print(int("9") + 7) #returns with integer 16
+
+print(float(7.3) + 4) #returns with float-point number 11.3
+
+
+
+
