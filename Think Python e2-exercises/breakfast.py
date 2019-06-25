30bde465b93e3103b2158557227ad1d3337230a4
Mustafa Erdogan <34981250+muerdogan@users.noreply.github.com>
2019-06-25 19:52:59 +0300

    Add files via upload

---
+++ Think Python e2-exercises/breakfast.py
@@ -0,0 +1,5 @@
+easy = 8*60+15
+tempo = 7*60+12
+breakfastt1_seconds= 2*easy + 3*tempo
+breakfastt2_min = breakfastt1_seconds / 60
+print(breakfastt2_min)
---
+++ Think Python e2-exercises/volume of sphere.py
@@ -0,0 +1,6 @@
+r = int(input("enter radius: "))
+pi = 3.14
+volume = (4/3)*pi*(r**3)
+print("volume is: ", volume)
+
+
